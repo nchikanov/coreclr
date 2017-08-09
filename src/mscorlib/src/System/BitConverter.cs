1928c5dae2269eb35ba54cf9e5c0bdc41c946082
t-nichik <t-nichik@microsoft.com>
2017-08-08 17:06:10 -0700

    Fix merge conflicts
    Forgot to update local codebase before making changes

--- src/mscorlib/src/System/BitConverter.cs
+++ src/mscorlib/src/System/BitConverter.cs
@@ -3,9 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
-using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
-using System.Security;
 
 namespace System
 {
@@ -43,9 +41,6 @@ namespace System
         // Converts a Boolean into an array of bytes with length one.
         public static byte[] GetBytes(bool value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == 1);
-
             byte[] r = new byte[1];
             r[0] = (value ? (byte)1 : (byte)0);
             return r;
@@ -58,9 +53,6 @@ namespace System
         // Converts a char into an array of bytes with length two.
         public static byte[] GetBytes(char value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(char));
-
             byte[] bytes = new byte[sizeof(char)];
             Unsafe.As<byte, char>(ref bytes[0]) = value;
             return bytes;
@@ -73,9 +65,6 @@ namespace System
         // two.
         public static byte[] GetBytes(short value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(short));
-
             byte[] bytes = new byte[sizeof(short)];
             Unsafe.As<byte, short>(ref bytes[0]) = value;
             return bytes;
@@ -88,9 +77,6 @@ namespace System
         // four.
         public static byte[] GetBytes(int value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(int));
-
             byte[] bytes = new byte[sizeof(int)];
             Unsafe.As<byte, int>(ref bytes[0]) = value;
             return bytes;
@@ -103,9 +89,6 @@ namespace System
         // eight.
         public static byte[] GetBytes(long value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(long));
-
             byte[] bytes = new byte[sizeof(long)];
             Unsafe.As<byte, long>(ref bytes[0]) = value;
             return bytes;
@@ -119,9 +102,6 @@ namespace System
         [CLSCompliant(false)]
         public static byte[] GetBytes(ushort value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(ushort));
-
             byte[] bytes = new byte[sizeof(ushort)];
             Unsafe.As<byte, ushort>(ref bytes[0]) = value;
             return bytes;
@@ -136,9 +116,6 @@ namespace System
         [CLSCompliant(false)]
         public static byte[] GetBytes(uint value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(uint));
-
             byte[] bytes = new byte[sizeof(uint)];
             Unsafe.As<byte, uint>(ref bytes[0]) = value;
             return bytes;
@@ -153,9 +130,6 @@ namespace System
         [CLSCompliant(false)]
         public static byte[] GetBytes(ulong value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(ulong));
-
             byte[] bytes = new byte[sizeof(ulong)];
             Unsafe.As<byte, ulong>(ref bytes[0]) = value;
             return bytes;
@@ -169,9 +143,6 @@ namespace System
         // four.
         public static byte[] GetBytes(float value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(float));
-
             byte[] bytes = new byte[sizeof(float)];
             Unsafe.As<byte, float>(ref bytes[0]) = value;
             return bytes;
@@ -184,9 +155,6 @@ namespace System
         // eight.
         public static byte[] GetBytes(double value)
         {
-            Contract.Ensures(Contract.Result<byte[]>() != null);
-            Contract.Ensures(Contract.Result<byte[]>().Length == sizeof(double));
-
             byte[] bytes = new byte[sizeof(double)];
             Unsafe.As<byte, double>(ref bytes[0]) = value;
             return bytes;
@@ -208,7 +176,6 @@ namespace System
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.startIndex, ExceptionResource.ArgumentOutOfRange_Index);
             if (startIndex > value.Length - sizeof(short))
                 ThrowHelper.ThrowArgumentException(ExceptionResource.Arg_ArrayPlusOffTooSmall, ExceptionArgument.value);
-            Contract.EndContractBlock();
 
             return Unsafe.ReadUnaligned<short>(ref value[startIndex]);
         }
@@ -221,7 +188,6 @@ namespace System
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.startIndex, ExceptionResource.ArgumentOutOfRange_Index);
             if (startIndex > value.Length - sizeof(int))
                 ThrowHelper.ThrowArgumentException(ExceptionResource.Arg_ArrayPlusOffTooSmall, ExceptionArgument.value);
-            Contract.EndContractBlock();
 
             return Unsafe.ReadUnaligned<int>(ref value[startIndex]);
         }
@@ -234,7 +200,6 @@ namespace System
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.startIndex, ExceptionResource.ArgumentOutOfRange_Index);
             if (startIndex > value.Length - sizeof(long))
                 ThrowHelper.ThrowArgumentException(ExceptionResource.Arg_ArrayPlusOffTooSmall, ExceptionArgument.value);
-            Contract.EndContractBlock();
 
             return Unsafe.ReadUnaligned<long>(ref value[startIndex]);
         }
@@ -301,54 +266,55 @@ namespace System
             return (char)(i - 10 + 'A');
         }
 
+        // Converts an array of bytes into a String.  
         public static string ToString(byte[] value, int startIndex, int length)
-        {
-            return ToString(new ReadOnlySpan<byte>(value, startIndex, length));
-        }
-
-            // Converts an array of bytes into a String.  
-            public static string ToString(ReadOnlySpan<byte> value)
         {
             if (value == null)
                 ThrowHelper.ThrowArgumentNullException(ExceptionArgument.value);
-            Contract.EndContractBlock();
+            if (startIndex < 0 || startIndex >= value.Length && startIndex > 0)
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.startIndex, ExceptionResource.ArgumentOutOfRange_Index);
+            if (length < 0)
+                throw new ArgumentOutOfRangeException(nameof(length), SR.ArgumentOutOfRange_GenericPositive);
+            if (startIndex > value.Length - length)
+                ThrowHelper.ThrowArgumentException(ExceptionResource.Arg_ArrayPlusOffTooSmall, ExceptionArgument.value);
 
-            if (value.Length == 0)
+            if (length == 0)
             {
                 return string.Empty;
             }
 
-            if (value.Length > (int.MaxValue / 3))
+            if (length > (int.MaxValue / 3))
             {
                 // (Int32.MaxValue / 3) == 715,827,882 Bytes == 699 MB
-                throw new ArgumentOutOfRangeException(nameof(value.Length), SR.Format(SR.ArgumentOutOfRange_LengthTooLarge, (int.MaxValue / 3)));
+                throw new ArgumentOutOfRangeException(nameof(length), SR.Format(SR.ArgumentOutOfRange_LengthTooLarge, (int.MaxValue / 3)));
             }
 
-            int chArrayLength = value.Length * 3;
+            int chArrayLength = length * 3;
             const int StackLimit = 512; // arbitrary limit to switch from stack to heap allocation
             unsafe
             {
                 if (chArrayLength < StackLimit)
                 {
                     char* chArrayPtr = stackalloc char[chArrayLength];
-                    return ToString(value, chArrayPtr, chArrayLength);
+                    return ToString(value, startIndex, length, chArrayPtr, chArrayLength);
                 }
                 else
                 {
                     char[] chArray = new char[chArrayLength];
                     fixed (char* chArrayPtr = &chArray[0])
-                        return ToString(value, chArrayPtr, chArrayLength);
+                        return ToString(value, startIndex, length, chArrayPtr, chArrayLength);
                 }
             }
         }
 
-        private static unsafe string ToString(ReadOnlySpan<byte> value, char* chArray, int chArrayLength)
+        private static unsafe string ToString(byte[] value, int startIndex, int length, char* chArray, int chArrayLength)
         {
-            Debug.Assert(value.Length > 0);
-            Debug.Assert(chArrayLength == value.Length * 3);
+            Debug.Assert(length > 0);
+            Debug.Assert(chArrayLength == length * 3);
 
             char* p = chArray;
-            for (int i = 0; i < value.Length; i++)
+            int endIndex = startIndex + length;
+            for (int i = startIndex; i < endIndex; i++)
             {
                 byte b = value[i];
                 *p++ = GetHexValue(b >> 4);
@@ -365,8 +331,6 @@ namespace System
         {
             if (value == null)
                 ThrowHelper.ThrowArgumentNullException(ExceptionArgument.value);
-            Contract.Ensures(Contract.Result<string>() != null);
-            Contract.EndContractBlock();
             return ToString(value, 0, value.Length);
         }
 
@@ -375,8 +339,6 @@ namespace System
         {
             if (value == null)
                 ThrowHelper.ThrowArgumentNullException(ExceptionArgument.value);
-            Contract.Ensures(Contract.Result<string>() != null);
-            Contract.EndContractBlock();
             return ToString(value, startIndex, value.Length - startIndex);
         }
 
@@ -400,7 +362,6 @@ namespace System
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.startIndex, ExceptionResource.ArgumentOutOfRange_Index);;
             if (startIndex > value.Length - 1)
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.startIndex, ExceptionResource.ArgumentOutOfRange_Index);; // differs from other overloads, which throw base ArgumentException
-            Contract.EndContractBlock();
 
             return value[startIndex] != 0;
         }
